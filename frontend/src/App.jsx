77328a5cec97fb4bf1cdca74d405423f1594efc8
Rishabh4Jakhar <rishabhjakhar04@gmail.com>
2025-04-13 23:52:32 +0530

    updating & testing style rn (looks bad)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
-import * as React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Landing from './pages/Landing';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar2";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import UserDashboard from './pages/UserDashboard';
-import QueryDashboard from './components/QueryDashboard';
-import './App.css';
+import UserDashboard from "./pages/UserDashboard";
+import QueryDashboard from "./components/QueryDashboard";
+import "./App.css";
+import DashboardLayout from "./pages/DashboardLayout";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/dashboard-layout" element={<DashboardLayout />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/query-dashboard" element={<QueryDashboard />} />
